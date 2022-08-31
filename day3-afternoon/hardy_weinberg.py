ad337b509d5881f453a6884066c068e508f2b5e6
Emily <emeier6@jh.edu>
2022-08-31 15:15:04 -0400

    first creation of py code

---
+++ day3-afternoon/hardy_weinberg.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+import matlablib.pyplot as plt
+
+from vcfParser import parse_vcf
+#same as: vcfParser.parse() and
+#import vcfParser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+parse_vcf()
+
+ 
+
+
