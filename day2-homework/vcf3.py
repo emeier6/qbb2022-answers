20d1b90c15de9cb1f07a6586be2177a8a16d521d
Emily <emeier6@jh.edu>
2022-08-31 10:45:29 -0400

    edited and updated close-as-possible final code to run - 31AUG22

--- day2-homework/vcf3.py
+++ day2-homework/vcf3.py
@@ -6,36 +6,40 @@ from vcfParser import *
 #print(parse_vcf('random_snippet.vcf'))
 
 randSNP = parse_vcf('random_snippet.vcf')
+db = parse_vcf('dbSNP_snippet.vcf') #Previous error:parse only needs the vcf file, not to be read
+#db = open('dbSNP_snippet.vcf', "r") #This would be able to read
 
-fs = open('dbSNP_snippet.vcf', "r")
+#If not parsing, will need to skip reading lines that start with "#", then also strip the lines and split the lines with tab. :
+    #if:
+    #   line.startswith("#"):
+    #       continue
+    #else:
+    #   fields = line.strip().split('\t')
 
 SNP_dict = {}
-for line in fs:
-    if line.startswith("#"):
+for line in db:
+    if line[0] == "CHROM":
         continue
-        
+    chrm = line[0]
+    pos = line[1]
+    identification = line[2]
+    
+    SNP_dict[(chrm, pos, identification)] = identification
+    
+counter = 0
+for i, things in enumerate(randSNP):
+    if things[0] == "CHROM":
+        continue
+    chrm = things[0]
+    pos = things[1]
+    identification = things[2]
+    
+    if (chrm, pos) in SNP_dict.keys():
+        things[2] = SNP_dict[(chrm, pos, identification)]
     else:
-        fields = line.strip().split('\t')
-        #finding out which is value and which is key
-        #we want the position as the key so that we can substitute the value (the ID) in for the aligned position
-        
-        #POS
-        fields[1] = int(fields[1])
-        pos = fields[1]
-        
-        #ID
-        identification = fields[2]
-        
-        SNP_dict[pos] = str(identification)
-            
+        counter = counter + 1
+        #conter =+ 1
+print(counter)        
 
-#key will be the ID
-#value will be the POSITION
+##OUTPUT: Says there are only 24 malformed entries.
 
-#sanity check
-for k, v in SNP_dict.items():
-    print(k, v)
-
-
-
-#for randSNP in 
