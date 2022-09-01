6238287127964c8ca1ce807b9a0e58f4ea9bf8ea
Emily <emeier6@jh.edu>
2022-09-01 14:51:53 -0400

    Annotations to subset regions for loop extraction

--- day4-lunch/subset_regions.sh
+++ day4-lunch/subset_regions.sh
@@ -9,12 +9,16 @@ then
     grep -w $CHR $GTF > $CHR.gtf
 fi
 
-for TYPE in protein_coding processed_pseudogene
+#create protein coding and processing genes, and anything you want to pull out
+for TYPE in lncRNA protein_coding processed_pseudogene
 do
     echo "--- Creating $TYPE.$CHR.bed"
     grep $TYPE $CHR.gtf | awk 'BEGIN {OFS="\t"}{if ($3=="gene"){print $1, $4-1, $5}}' > $TYPE.$CHR.bed
 done
 
+#creating an exons file
+#removing priotein coding or lncRNA genes that are not protein encoding (introns)
 echo "--- Creating exons.$CHR.bed"
-grep "protein_coding" $CHR.gtf | awk 'BEGIN {OFS="\t"}{if ($3=="exon"){print $1, $4-1, $5}}' > exons.$CHR.bed
+grep "lncRNA" $CHR.gtf | awk 'BEGIN {OFS="\t"}{if ($3=="exon"){print $1, $4-1, $5}}' > exons.$CHR.bed
 
+#python do_all.sh gencode.v41.annotation.gtf 
