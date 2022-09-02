f2c60f3399c1acecbae8af2269a352b18c22e5f3
Emily <emeier6@jh.edu>
2022-09-01 22:08:44 -0400

    End of day upload

--- day4-homework/binomial_power_interactive_lecture.py
+++ day4-homework/binomial_power_interactive_lecture.py
@@ -3,8 +3,15 @@
 import numpy
 from scipy.stats import binomtest
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 from statsmodels.stats.multitest import multipletests
+
+#add these two to the top, since we are defining the variables we are running
+tosses = numpy.array([10, 50, 100, 250, 500, 1000])
+probs = numpy.around(numpy.arange(0.55, 1.05, 0.05), decimals=2)[::-1]
+#print(numpy.arange(0.55, 1.05, 0.05))
+
+#print(probs)
 
 def simulate_coin_toss(n_tosses, prob_heads = 0.5, seed=None):
     '''
@@ -14,7 +21,7 @@ def simulate_coin_toss(n_tosses, prob_heads = 0.5, seed=None):
            seed, an integer, the random seed that will be used in the simulation; default is None
     Output: results_arr, an array of 1's and 0's with 1's representing heads and 0's representing tails
     Resources: https://numpy.org/doc/stable/reference/random/generated/numpy.random.choice.html
-    '''
+    '''    
     if seed is not None:
         numpy.random.seed(seed)
     results_arr = numpy.random.choice([0,1], size=n_tosses, p = [1-prob_heads, prob_heads])
@@ -85,6 +92,52 @@ def run_experiment(prob_heads, n_toss, n_iters = 100, seed = 389, correct_the_pv
     return(power)
 
 
+    
+#new array assigning start value at 0 for both probablility and tosses. Then, we define the length of the array with len for probabilities and for tosses.
+
+
+#Taking the single output and now putting it in a larger combined list
+#we are using the definitions from power to 
+#the double for loop loops first through probability list, and then the second for loop add the search for tosses
+#but, we are only looking to relate the single line or value of each of the probabilities, and for each of the tosses. The tosses is a list, but we are not filling in a list for each of the matrix points, just a single point that was defined as the single toss variable. The same is true for probs and single probs. then, we can print the power_twoD to see if we ran this correctly.
+power_twoD_Corr = numpy.zeros((len(probs), len(tosses)))
+for i, singleprob in enumerate(probs):
+    for j, singletoss in enumerate(tosses):
+        power_twoD_Corr[i,j] = run_experiment(singleprob, singletoss, correct_the_pvalues = True)
+#CHECK:print(power_twoD)
+#p<0.05 / number of thigs we are testing
+#this can be done by iterating through, which is what we are doing here...
+
+power_twoD = numpy.zeros((len(probs), len(tosses)))
+for i, singleprob2 in enumerate(probs):
+    for j, singletoss2 in enumerate(tosses):
+        power_twoD[i,j] = run_experiment(singleprob2, singletoss2)    
+    
+  
+
 power1 = run_experiment(0.6, 500, correct_the_pvalues = True)
+#print(power1)
 power2 = run_experiment(0.95, 10, correct_the_pvalues = True)
+#print(power2)
 power2b = run_experiment(0.95, 10)
+#print(power2b)
+
+
+
+#Creating a heat map plot of the matrixed values = power_twoD
+fig, (ax1, ax2) = plt.subplots(ncols=2)
+sns.heatmap(power_twoD_Corr, ax=ax1, vmin = 0, vmax = 1, annot=True, cbar=False, annot_kws={"size": 7}, xticklabels=tosses, yticklabels=probs, cmap='cubehelix')
+ax1.set_xlabel("Array of tosses")
+ax1.set_ylabel("Array of probabilities")    
+ax1.set_title("Corrected power matrix of \n (probability v. tosses) arrays")
+
+sns.heatmap(power_twoD, vmin = 0, vmax = 1, ax=ax2, annot=True, cbar=False, annot_kws={"size": 7}, xticklabels=tosses, yticklabels=probs, cmap='cubehelix')
+ax2.set_xlabel("Array of tosses")
+ax2.set_ylabel("Array of probabilities")    
+ax2.set_title("Uncorrected power matrix of \n (probability v. tosses) arrays")
+
+#plt.show()
+
+plt.savefig("Comp_Corr_NonCorr.png")
+plt.close(fig)
+
