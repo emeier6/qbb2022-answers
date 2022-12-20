3d626631dcd08ef701402717698a93f8f8724a0c
Emily <emeier6@jh.edu>
2022-12-19 23:23:43 -0500

    the working python document

--- week14-homework/working.py
+++ week14-homework/working.py
@@ -2,6 +2,8 @@
 
 #USAGE: python scriptname.py KRAKEN_sample_input_file.kraken sample_name
 #EXAMPLE: python make_krona_compatible.py KRAKEN/SRR492183.kraken SRR492183
+# python working.py ~/qbb2022-answers/week14-homework/metagenomics_data/step0_givendata/KRAKEN/assembly.kraken ~/qbb2022-answers/week14-homework/bins_dir/bin2.fa
+
 
 import sys
 
@@ -9,8 +11,12 @@ f = open(sys.argv[1])
 sample = sys.argv[2]
 out = open(sample + "_krona.txt", "w")
 
-
 for line in f:
-    fields = line.strip('\r\n').split(';')
-    out.write("\t".join(fields[1:]) + "\n")
-    
+    new = sample.split('>')
+print(new[2])
+#
+# for line in f:
+#     fields = line.strip('\r\n').split('>')
+#     out.write("\t".join(fields[1:]) + "\n")
+#
+#I also can't figure this out but I am trying really hard :(
